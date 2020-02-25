0fb4395baf9b6b9e130698ea413635b2456d81f5
ridoyraihan <ridoyraihan@gmail.com>
2020-02-25 21:32:24 +0600

    Board sorted to positiontoid fixed

--- src/app/components/game-board/game-board.component.ts
+++ src/app/components/game-board/game-board.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Slot } from 'src/app/model/slot.model';
 
 @Component({
   selector: 'app-game-board',
   templateUrl: './game-board.component.html',
   styleUrls: ['./game-board.component.css']
 })
-export class GameBoardComponent implements OnInit {
+export class GameBoardComponent implements OnInit, OnChanges {
 
-  // @Input() boardConfig:
+  @Input() board: Slot[] = [];
 
   constructor() { }
+
+  ngOnChanges( change: SimpleChanges){
+    const board = <Slot[]>change.board.currentValue;
+    this.board = board;  
+    console.log(this.board)  
+  }
 
   ngOnInit() {
   }
